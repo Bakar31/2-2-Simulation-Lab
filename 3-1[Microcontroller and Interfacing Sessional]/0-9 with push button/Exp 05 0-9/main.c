5c286417d3749a39b97ba2e0db9ef97ec2d10d46
Bakar31 <abubakar1808031@gmail.com>
2022-09-29 13:06:06 +0600

    add projects

---
+++ 3-1[Microcontroller and Interfacing Sessional]/0-9 with push button/Exp 05 0-9/main.c
@@ -0,0 +1,100 @@
+/*
+ * Exp-05 0-9 with CC type 7-Segment.c
+ *
+ * Created: 6/8/2022 10:00:49 PM
+ * Author : G-A2
+ 
+ Goal: This project aims to increment value from 0-9 with the help a push button. The 'DisplayDigit' shows the desired value in the 7 segment CC 
+ type  display. POTRC is selected as the output terminal which is connected to the 7-segment display. The 1st pin of PORTD takes input from the push
+ button. This code is only to display number from 0-9. If the pushbutton is pressed more after reaching 9, it will not do anything.
+ 
+ This code is clean and we avoided complex statement to keep it simple. 
+ This is a highly reproducible code.
+ */ 
+
+#define F_CPU 8000000UL // set the CPU frequency at 8Mhz
+#include <avr/io.h>
+#include <util/delay.h>
+
+static void DisplayDigit(uint8_t num); //Function Prototype
+
+
+int main(void) {
+    DDRC = 0xFF; // PORTC as Output
+	DDRD &= ~(1<<PD0); // First pin of PORTD as Input
+	
+	uint8_t count = 0; 	// Variable that counts the Input
+	DisplayDigit(count); // Show Initial count value (0) on Display
+	
+    while (1) {
+		
+		if( (PIND & 0x01) == 0 ) // Check if switch is pressed
+		{
+			_delay_ms(80); // delay for 80 s
+			
+			if (count < 9 ) // Check if count value is less than MAX value (9)
+			{
+				count++; // If true increment the count value
+			}
+				
+			else
+			{
+				continue; // Otherwise continue
+			}
+		
+			DisplayDigit(count); // Show the count value on Display
+		}
+    }
+	
+	return 0;
+}
+ 
+static void DisplayDigit(uint8_t num) {
+	
+	PORTC &= 0x80; // Clear only bits (PC6:PC0)
+	switch (num) 
+	{
+		// Print 0 on 7-segment if num = 0 or num > 9
+		default:
+		
+		case 0:
+			PORTC |= 0x3F; // show 0
+			break;
+		
+		case 1:
+			PORTC |= 0x06; // show 1
+			break;
+			
+		case 2:
+			PORTC |= 0x5B; // show 2
+			break;
+			
+		case 3:
+			PORTC |= 0x4F; // show 3
+			break;
+			
+		case 4:
+			PORTC |= 0x66; // show 4
+			break;
+			
+		case 5:
+			PORTC |= 0x6D; // show 5
+			break;
+		
+		case 6:
+			PORTC |= 0x7D; // show 6
+			break;
+		
+		case 7:
+			PORTC |= 0x07; // show 7
+			break;
+		
+		case 8:
+			PORTC |= 0x7F; // show 8
+			break;
+		
+		case 9:
+			PORTC |= 0x6F; // show 9
+			break;
+	}
+}
